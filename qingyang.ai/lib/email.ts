e9412ca6a0ca5b99db5cfa9d766570f20dd1fc13
LinQingYang <code@qingyang.ai>
2025-09-04 23:12:24 +0800

    top

--- qingyang.ai/lib/email.ts
+++ qingyang.ai/lib/email.ts
@@ -17,7 +17,7 @@ const {
   SMTP_HOST = 'smtp.exmail.qq.com',
   SMTP_PORT = '465',
   SMTP_SECURE = 'true',
-  SMTP_USER = 'contact@qingyang.ai',
+  SMTP_USER = 'liufeng@datagov.top',
   SMTP_PASS = '',
   MAIL_FROM = `qingyang.ai 咨询系统 <contact@qingyang.ai>`,
   MAIL_TO = '15274452497@163.com',
