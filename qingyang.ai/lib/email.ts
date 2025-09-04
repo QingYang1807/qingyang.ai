d4a36e556c3b7f2c5b3e105cbf131bdee4ca10ec
LinQingYang <code@qingyang.ai>
2025-09-04 23:17:06 +0800

    SET ENV SMTP_PASS

--- qingyang.ai/lib/email.ts
+++ qingyang.ai/lib/email.ts
@@ -18,7 +18,7 @@ const {
   SMTP_PORT = '465',
   SMTP_SECURE = 'true',
   SMTP_USER = 'liufeng@datagov.top',
-  SMTP_PASS = '',
+  SMTP_PASS = process.env.SMTP_PASS,
   MAIL_FROM = `qingyang.ai 咨询系统 <contact@qingyang.ai>`,
   MAIL_TO = '15274452497@163.com',
 } = process.env;
